7f1ef7615a8b3034030c460d68c79cca3f3c8f4f
JontAmazon <jonatan.lindholm10@gmail.com>
2025-07-15 22:06:20 +0200

    Refactor LoginPage

--- parabank/LoginPage.js
+++ parabank/LoginPage.js
@@ -1,12 +1,14 @@
 class LoginPage {
   constructor(page) {
     this.page = page;
+    this.BASE_URL = 'https://parabank.parasoft.com/parabank/index.htm';
     this.usernameInput = page.locator('input[name="username"]');
     this.passwordInput = page.locator('input[name="password"]');
     this.loginButton = page.locator('input[value="Log In"]');
   }
 
   async login(username = 'JohnSmith42', password = 'password') {
+    await this.page.goto(this.BASE_URL);
     await this.usernameInput.fill(username);
     await this.passwordInput.fill(password);
     await this.loginButton.click();
--- parabank/login.spec.js
+++ parabank/login.spec.js
@@ -3,11 +3,6 @@ const LoginPage = require('./LoginPage');
 
 test('Test Case 1: Log in', async ({ page }) => {
   const loginPage = new LoginPage(page);
-  const BASE_URL = 'https://parabank.parasoft.com/parabank/index.htm';
-  const USERNAME = `JohnSmith`;
-  const PASSWORD = 'password';
-
-  await page.goto(BASE_URL);
   await loginPage.login();
   await expect(page.getByRole('heading', { name: 'Accounts Overview' })).toBeVisible();
   await expect(page.getByText('Log Out')).toBeVisible();
